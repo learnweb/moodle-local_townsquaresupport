e4c9ef6e76936d0f3b48da9fc57d635e86fe2f99
tamaro <tamarowalter@yahoo.de>
2025-07-06 23:38:05 +0200

    code cleaning

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2024062400;
-$plugin->requires = 2022112819; // Require Moodle 4.1
+$plugin->requires = 2022112819; // Require Moodle 4.1.
 $plugin->supported = [401, 404, 405, 500];
 $plugin->component = 'local_townsquaresupport';
 $plugin->maturity = MATURITY_RC;
